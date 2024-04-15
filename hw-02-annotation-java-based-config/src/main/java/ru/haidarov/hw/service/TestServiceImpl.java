88e24ea61eaa4d41cb066f69ee1e853e0f4340e0
Ivan Haydarov <navi1435@gmail.com>
2024-04-15 23:31:53 +0700

    initial commit for hw-02

---
+++ hw-02-annotation-java-based-config/src/main/java/ru/haidarov/hw/service/TestServiceImpl.java
@@ -0,0 +1,29 @@
+package ru.haidarov.hw.service;
+
+import lombok.RequiredArgsConstructor;
+import ru.haidarov.hw.dao.QuestionDao;
+import ru.haidarov.hw.domain.Question;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class TestServiceImpl implements TestService {
+
+    private final IOService ioService;
+
+    private final QuestionDao questionDao;
+
+    @Override
+    public void executeTest() {
+        ioService.printLine("");
+        ioService.printFormattedLine("Please answer the questions below%n");
+        List<Question> questions = questionDao.findAll();
+        questions.forEach(question -> {
+            ioService.printLine(question.text());
+            for (int i = 0; i < question.answers().size(); i++) {
+                String answerText = question.answers().get(i).text();
+                ioService.printFormattedLine("%d) " + answerText, i + 1);
+            }
+        });
+    }
+}
