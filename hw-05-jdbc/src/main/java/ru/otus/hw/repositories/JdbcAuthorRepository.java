4731291f650a5d8f3ea4bda89639aa5a99218b27
Ivan Haydarov <navi1435@gmail.com>
2024-05-26 00:16:32 +0700

    library app on spring jdbc created

---
+++ hw-05-jdbc/src/main/java/ru/otus/hw/repositories/JdbcAuthorRepository.java
@@ -0,0 +1,43 @@
+package ru.otus.hw.repositories;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+import org.springframework.stereotype.Repository;
+import ru.otus.hw.models.Author;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class JdbcAuthorRepository implements AuthorRepository {
+
+    private final NamedParameterJdbcOperations jdbc;
+
+    @Override
+    public List<Author> findAll() {
+        String findAll = "SELECT id, full_name FROM authors";
+        return jdbc.query(findAll, new AuthorRowMapper());
+    }
+
+    @Override
+    public Optional<Author> findById(long id) {
+        String findById = "SELECT id, full_name FROM authors WHERE id = :id";
+        return Optional.ofNullable(jdbc.queryForObject(
+                findById, Map.of("id", id), new AuthorRowMapper()));
+    }
+
+    private static class AuthorRowMapper implements RowMapper<Author> {
+
+        @Override
+        public Author mapRow(ResultSet rs, int i) throws SQLException {
+            long id = rs.getLong("id");
+            String fullName = rs.getString("full_name");
+            return new Author(id, fullName);
+        }
+    }
+}
