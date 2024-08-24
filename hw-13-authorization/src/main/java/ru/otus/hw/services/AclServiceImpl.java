20f2982631c167e2e5308989d7cc95c2c8961681
Ivan Haydarov <navi1435@gmail.com>
2024-08-25 00:15:44 +0700

    acl setup moved to AclService

---
+++ hw-13-authorization/src/main/java/ru/otus/hw/services/AclServiceImpl.java
@@ -0,0 +1,52 @@
+package ru.otus.hw.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.acls.domain.BasePermission;
+import org.springframework.security.acls.domain.GrantedAuthoritySid;
+import org.springframework.security.acls.domain.ObjectIdentityImpl;
+import org.springframework.security.acls.domain.PrincipalSid;
+import org.springframework.security.acls.model.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import ru.otus.hw.models.Book;
+
+@Service
+@RequiredArgsConstructor
+public class AclServiceImpl implements AclService {
+
+    private final MutableAclService mutableAclService;
+
+    public MutableAcl setUpAclFor(Book book) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        final Sid owner = new PrincipalSid(authentication);
+        ObjectIdentity oid = new ObjectIdentityImpl(book);
+        MutableAcl acl;
+        try {
+            acl = (MutableAcl) mutableAclService.readAclById(oid);
+        } catch (NotFoundException nfe) {
+            acl = mutableAclService.createAcl(oid);
+        }
+        acl.setOwner(owner);
+
+        final Sid admin = new GrantedAuthoritySid("ROLE_ADMIN");
+        final Sid user = new GrantedAuthoritySid("ROLE_USER");
+        final Sid adult = new GrantedAuthoritySid("ROLE_ADULT_USER");
+
+        acl.insertAce(acl.getEntries().size(), BasePermission.ADMINISTRATION, admin, true);
+        removeAce(acl, BasePermission.READ, user);
+        acl.insertAce(acl.getEntries().size(), BasePermission.READ, user, !book.isAdultOnly());
+        acl.insertAce(acl.getEntries().size(), BasePermission.READ, adult, true);
+        return acl;
+    }
+
+    private void removeAce(MutableAcl acl, Permission permission, Sid sid) {
+        for (int i = 0; i < acl.getEntries().size(); i++) {
+            AccessControlEntry ace = acl.getEntries().get(i);
+            if (ace.getSid().equals(sid) && ace.getPermission().equals(permission)) {
+                acl.deleteAce(i);
+                break;
+            }
+        }
+    }
+}
--- hw-13-authorization/src/main/java/ru/otus/hw/services/BookServiceImpl.java
+++ hw-13-authorization/src/main/java/ru/otus/hw/services/BookServiceImpl.java
@@ -1,19 +1,6 @@
 package ru.otus.hw.services;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.acls.domain.BasePermission;
-import org.springframework.security.acls.domain.GrantedAuthoritySid;
-import org.springframework.security.acls.domain.ObjectIdentityImpl;
-import org.springframework.security.acls.domain.PrincipalSid;
-import org.springframework.security.acls.model.AccessControlEntry;
-import org.springframework.security.acls.model.MutableAcl;
-import org.springframework.security.acls.model.MutableAclService;
-import org.springframework.security.acls.model.NotFoundException;
-import org.springframework.security.acls.model.ObjectIdentity;
-import org.springframework.security.acls.model.Permission;
-import org.springframework.security.acls.model.Sid;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.otus.hw.exceptions.EntityNotFoundException;
@@ -38,7 +25,7 @@ public class BookServiceImpl implements BookService {
 
     private final BookRepository bookRepository;
 
-    private final MutableAclService mutableAclService;
+    private final AclService aclService;
 
     @Override
     @Transactional(readOnly = true)
@@ -57,7 +44,7 @@ public class BookServiceImpl implements BookService {
     @Transactional
     public Book insert(String title, long authorId, Set<Long> genresIds, boolean adultOnly) {
         Book book = save(0, title, authorId, genresIds, adultOnly);
-        setUpAclFor(book);
+        aclService.setUpAclFor(book);
         return book;
     }
 
@@ -69,7 +56,7 @@ public class BookServiceImpl implements BookService {
             throw new EntityNotFoundException("Book for update with id %d is not present".formatted(id));
         }
         Book updatedBook = save(id, title, authorId, genresIds, adultOnly);
-        setUpAclFor(updatedBook);
+        aclService.setUpAclFor(updatedBook);
         return updatedBook;
     }
 
@@ -96,38 +83,5 @@ public class BookServiceImpl implements BookService {
 
         var book = new Book(id, title, author, genres, adultOnly);
         return bookRepository.save(book);
-    }
-
-    private MutableAcl setUpAclFor(Book book) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        final Sid owner = new PrincipalSid(authentication);
-        ObjectIdentity oid = new ObjectIdentityImpl(book);
-        MutableAcl acl;
-        try {
-            acl = (MutableAcl) mutableAclService.readAclById(oid);
-        } catch (NotFoundException nfe) {
-            acl = mutableAclService.createAcl(oid);
-        }
-        acl.setOwner(owner);
-
-        final Sid admin = new GrantedAuthoritySid("ROLE_ADMIN");
-        final Sid user = new GrantedAuthoritySid("ROLE_USER");
-        final Sid adult = new GrantedAuthoritySid("ROLE_ADULT_USER");
-
-        acl.insertAce(acl.getEntries().size(), BasePermission.ADMINISTRATION, admin, true);
-        removeAce(acl, BasePermission.READ, user);
-        acl.insertAce(acl.getEntries().size(), BasePermission.READ, user, !book.isAdultOnly());
-        acl.insertAce(acl.getEntries().size(), BasePermission.READ, adult, true);
-        return acl;
-    }
-
-    private void removeAce(MutableAcl acl, Permission permission, Sid sid) {
-        for (int i = 0; i < acl.getEntries().size(); i++) {
-            AccessControlEntry ace = acl.getEntries().get(i);
-            if (ace.getSid().equals(sid) && ace.getPermission().equals(permission)) {
-                acl.deleteAce(i);
-                break;
-            }
-        }
     }
 }
